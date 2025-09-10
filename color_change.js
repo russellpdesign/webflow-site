54932181697da9ab11859950595670341080393e
Russell Pritchard <russell@Mac.lan>
2025-09-09 17:45:07 -0700

    upated color change

---
+++ color_change.js
@@ -0,0 +1,22 @@
+const pageBody = document.querySelector(".body");
+const pageHeight = pageBody.scrollHeight;
+const windowSize = window.innerHeight;
+const pageOffset = pageHeight - windowSize;
+
+function changeColor(e) {
+  position = window.pageYOffset;
+  console.log(position);
+  footerSwitch = Math.round(document.body.scrollHeight - windowSize * 1.5);
+  heroSwitch = Math.round(window.innerHeight * 0.4);
+
+  if (position > heroSwitch) {
+    pageBody.classList.add("u-theme-dark");
+    console.log("the dark theme has been triggered!");
+  }
+  if ((position > footerSwitch) | (position < heroSwitch)) {
+    pageBody.classList.remove("u-theme-dark");
+    console.log("The base mode has been triggered!");
+  }
+}
+
+window.addEventListener("scroll", changeColor);
