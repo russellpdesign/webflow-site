469c7bef28a9c0a7cbd3adc5a2ed92825c1c85ab
russellpdesign <70049636+russellpdesign@users.noreply.github.com>
2025-11-21 14:15:41 -0800

    added period before classname in dropdownRowsClassName

--- dropdown-accordian.js
+++ dropdown-accordian.js
@@ -2,7 +2,7 @@
 (function myFunction() {
   
   // class of the row container
-  const dropdownRowsClassName = "dropdown-header-container.is-active";
+  const dropdownRowsClassName = ".dropdown-header-container.is-active";
   // nodelist of the rows
   // const dropdownContainerNodeList = document.querySelectorAll(`.${dropdownRowsClassName}`);
   const dropdownRows = Array.from(document.querySelectorAll(`.${dropdownRowsClassName}`));
