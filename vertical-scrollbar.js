42020a582fba0361be71db0414627f944f35836b
russellpdesign <70049636+russellpdesign@users.noreply.github.com>
2025-11-20 18:22:23 -0800

    final commit

--- vertical-scrollbar.js
+++ vertical-scrollbar.js
@@ -1,10 +1,12 @@
 function moveScrollBar() {
 
+    // DOM ELEMENTS
     const elementToAnimateClassName = "vertical-progress-bar-inside";
-
     const elementToAnimate = document.querySelector(`.${elementToAnimateClassName}`);
-    // since our element is sticky, we need to figure out the scroll height of the section by targeting the overview_trigger elements and adding up their cumulative height
     const triggers = document.querySelectorAll(".overview_trigger");
+
+    // DISTANCES AND TRIGGER POINTS
+    // since our element is sticky, we need to figure out the scroll height of the section by targeting the overview_trigger elements and adding up their cumulative height
     const triggersHeight = triggers[0].getBoundingClientRect().height * triggers.length;
     const currentPosition = window.scrollY;
     const viewportHeight = window.innerHeight;
@@ -12,6 +14,11 @@ function moveScrollBar() {
     const startTrigger = triggers[0].getBoundingClientRect().top - viewportHeight - document.body.getBoundingClientRect().top;
     // scrollbar should stop animating when the top of our last trigger reaches the top of the viewport
     const endTrigger = startTrigger + triggersHeight;
+
+    // ANIMATION FORMULAS
+    const yPercent = (((currentPosition - startTrigger) / (endTrigger - startTrigger)) * 100) * 2;
+
+    /* to troubleshoot issues with this script, use the following
 
     const vertScrollBarStats = {
         elementToAnimateClassName: `${elementToAnimateClassName}`,
@@ -21,16 +28,15 @@ function moveScrollBar() {
         viewportHeight: `${viewportHeight}`,
         startTrigger: `${startTrigger}`,
         endTrigger: `${endTrigger}`,
+        yPercent: `${yPercent}`,
     }
 
     console.log("------ Vertical Scrolling Slider Bar ------");
     console.table(vertScrollBarStats);
+    */
 
         // scrollbar handling
         if ( currentPosition >= startTrigger && currentPosition <= endTrigger ) {
-        console.log("Animate scroll bar!");
-        const yPercent = (((currentPosition - startTrigger) / (endTrigger - startTrigger)) * 100) * 2;
-        console.log(`${yPercent}`);
         elementToAnimate.style.transform = `translate3d(0, ${yPercent}%, 0)`
         }
 
@@ -43,7 +49,7 @@ function moveScrollBar() {
         }
 }
 
-const throttledScrollBar = throttle(moveScrollBar, 15); // Throttle to 100ms
+const throttledScrollBar = throttle(moveScrollBar, 15); // Throttle to 15ms
 
 
 document.addEventListener("scroll", throttledScrollBar);
