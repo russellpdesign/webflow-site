7463bc05fe5e660ac971c21c0a718cf285e54a00
russellpdesign <70049636+russellpdesign@users.noreply.github.com>
2025-11-20 17:10:20 -0800

    removed throttle helper function

--- vertical-scrollbar.js
+++ vertical-scrollbar.js
@@ -1,26 +1,26 @@
-function throttle(func, delay) {
-  let timeoutId;
-  let lastArgs;
-  let lastThis;
-  let lastExecTime = 0;
+// function throttle(func, delay) {
+//   let timeoutId;
+//   let lastArgs;
+//   let lastThis;
+//   let lastExecTime = 0;
 
-  return function(...args) {
-    lastArgs = args;
-    lastThis = this;
-    const currentTime = Date.now();
+//   return function(...args) {
+//     lastArgs = args;
+//     lastThis = this;
+//     const currentTime = Date.now();
 
-    if (currentTime - lastExecTime >= delay) {
-      lastExecTime = currentTime;
-      func.apply(lastThis, lastArgs);
-    } else if (!timeoutId) {
-      timeoutId = setTimeout(() => {
-        lastExecTime = Date.now();
-        func.apply(lastThis, lastArgs);
-        timeoutId = null;
-      }, delay - (currentTime - lastExecTime));
-    }
-  };
-}
+//     if (currentTime - lastExecTime >= delay) {
+//       lastExecTime = currentTime;
+//       func.apply(lastThis, lastArgs);
+//     } else if (!timeoutId) {
+//       timeoutId = setTimeout(() => {
+//         lastExecTime = Date.now();
+//         func.apply(lastThis, lastArgs);
+//         timeoutId = null;
+//       }, delay - (currentTime - lastExecTime));
+//     }
+//   };
+// }
 
 function moveScrollBar() {
 
